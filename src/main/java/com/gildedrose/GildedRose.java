8350c576a6fd27eead9eccd6373bd16336191ffb
Arthur <arthurproust1@gmail.com>
2019-11-04 16:19:24 +0100

    minor fix

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -59,7 +59,7 @@ public class GildedRose {
             if (item.sellIn < 0 && item.quality > 0) {
                 item.quality--;
             }
-            if (item.name.contains("Conjured")  && item.quality > 0) {
+            if (item.name.equals("Conjured Mana Cake") && item.name.equals("Conjured Chocolate Cake") && item.quality > 0) {
                 item.quality = ConjuredThings(item);
             }
         }
