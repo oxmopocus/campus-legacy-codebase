4e90e2a5841ef7f5f30ceeefdfec8244dbf6ce47
Arthur <arthurproust1@gmail.com>
2019-11-04 15:58:41 +0100

    minor fix

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,20 +1,14 @@
 package com.gildedrose;
-
 import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
-
 class GildedRoseTest {
-/*
     @Test
     void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertThat(app.items[0].name).isEqualTo("fixme");
+        assertThat(app.items[0].name).isEqualTo("foo");
     }
-*/
-
     @Test
     void cheese() {
         Item[] items = new Item[] { new Item("Aged Brie", 2, 0) };
@@ -22,8 +16,6 @@ class GildedRoseTest {
         app.updateQuality();
         assertThat(app.items[0].sellIn).isEqualTo(1);
     }
-
-
     @Test
     void concert() {
         Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) };
@@ -32,5 +24,3 @@ class GildedRoseTest {
         assertThat(app.items[0].quality).isNotZero();
     }
 }
-
-
