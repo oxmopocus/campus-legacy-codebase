79efb49bfe0059f47169364a0ad94d049d7b496b
Arthur <arthurproust1@gmail.com>
2019-11-05 14:08:16 +0100

    change code JP

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,12 +1,33 @@
 package com.gildedrose;
 import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
 
 
 class GildedRoseTest {
 
-    //Regular Names
+    @Test
+    void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].name).isEqualTo("foo");
+    }
+
+    @Test
+    void cheese() {
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].sellIn).isEqualTo(1);
+    }
+
+    @Test
+    void concert() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isNotZero();
+    }
 
     @Test
     void foo_decrease_Sellin_All_Day() {
@@ -201,6 +222,7 @@ class GildedRoseTest {
         GildedRose app = new GildedRose(items);
         assertThat(app.qualityBounded(55,-2)).isEqualTo(50);
     }
+
 
 
 }
