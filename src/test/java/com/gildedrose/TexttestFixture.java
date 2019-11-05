1e89c190294c8873b94cba2fb59656f3fbf1964c
Arthur <arthurproust1@gmail.com>
2019-11-05 13:59:24 +0100

    minor fix

--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -14,6 +14,7 @@ public class TexttestFixture {
                 // this conjured item does not work properly yet
                 new Item("Conjured Mana Cake", 3, 6) ,
                 new Item ("Conjured Chocolate Cake", 3, 6)};
+
         GildedRose app = new GildedRose(items);
         int days = 2;
         if (args.length > 0) {
